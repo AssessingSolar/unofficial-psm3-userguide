9b1a9a5d45f13885ddb6ae7eb06d658032b9f52b
Kevin Anderson <kevin.anderson@nrel.gov>
2022-09-26 13:28:03 -0400

    add some initial content and sphinx config

---
+++ source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "The Unofficial PSM3 User's Guide"
+copyright = '2022, Kevin Anderson'
+author = 'Kevin Anderson'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['nbsphinx']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+nbsphinx_execute = 'never'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
